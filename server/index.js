6f939c33588c8cd53af752d5a8f46ef0f3522d74
mindychxn <mindychxn@gmail.com>
2024-01-25 14:04:04 -0500

    Add middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json()); // for parsing JSON in the request body
 
 app.listen(3000, () => {
   console.log("Server started on port 3000...");
