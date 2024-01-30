2c7b185576c2ef84cbd6edef14620779b92e13b4
mindychxn <mindychxn@gmail.com>
2024-01-30 00:35:59 -0500

    Add delete functionality

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([]);
+
+  // get all todos
   const getTodos = async () => {
     try {
       const response = await fetch('http://localhost:3000/todos');
@@ -15,20 +17,28 @@ export default function TodoList() {
   useEffect(() => {
     getTodos();
   }, []);
+
+  // delete function
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:3000/todos/${id}`, { method: 'DELETE' });
+      setTodos(todos.filter((todo) => todo.id !== id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="flex flex-col gap-4 w-3/4">
       <div className="font-semibold">List To Dos</div>
-      {todos.map((todo, i) => (
-        <div key={i} className="flex justify-between w-full bg-white py-2 px-4 rounded-lg">
+      {todos.map((todo) => (
+        <div key={todo.id} className="flex justify-between w-full bg-white py-2 px-4 rounded-lg">
           <label className="flex gap-2">
             <input type="checkbox" />
-            <div className="">
-              {todo.description.charAt(0).toUpperCase() + todo.description.slice(1)}
-            </div>
+            <div>{todo.description.charAt(0).toUpperCase() + todo.description.slice(1)}</div>
           </label>
           <div className="flex gap-2">
             <button className="bg-blue-100">Edit</button>
-            <button>Delete</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
           </div>
         </div>
       ))}
