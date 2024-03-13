6a1ef586c852c7435fe52605c5c28e24cf8a9252
mindychxn <mindychxn@gmail.com>
2024-03-12 22:15:41 -0400

    Change imports to commonJS

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+const pg = require('pg');
+const { Pool } = pg;
 
 const pool = new Pool({
   user: 'todo_user',
   password: 'password',
   host: 'localhost',
-  database: 'perntodo'
-})
+  database: 'perntodo',
+});
 
-export default pool;
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-import pool from './db.js';
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // middleware
@@ -9,61 +8,10 @@ app.use(express.json()); // for parsing JSON in the request body
 
 // ROUTES
 
-// create a todo
-app.post('/todos', async (req, res) => {
-  try {
-    const { description } = req.body; // get description value from request body 
-    // insert description fro req body into todo table, return data
-    const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *', [description]);
-    res.json(newTodo.rows[0]); // send response of newly added todo
-  } catch (err) { 
-    console.error(err);
-  }
-})
+app.use('/auth', require('./routes/jwtAuth'));
 
-// get all todos
-app.get('/todos', async (req, res) => {
-  try {
-    const allTodos = await pool.query('SELECT * FROM todo');
-    res.json(allTodos.rows);
-  } catch (err) { 
-    console.error(err);
-  }
-})
+app.use('/todos', require('./routes/todos'));
 
-// get a todo
-app.get('/todos/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const todo = await pool.query('SELECT * FROM todo WHERE id = $1', [id]);
-    res.json(todo.rows[0]);
-  } catch (err) { 
-    console.error(err);
-  }
-})
-
-// update a todo
-app.put('/todos/:id', async (req, res) => {
-  try {
-    const { id } = req.params; // get id from url param
-    const { description } = req.body; // get updated decription from body of req
-    const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE id = $2 RETURNING *', [description, id])
-    res.json(updateTodo.rows[0]);
-  } catch (err) { 
-    console.error(err);
-  }
-})
-
-// delete a todo
-app.delete('/todos/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const deleteTodo = await pool.query('DELETE FROM todo WHERE id = $1', [id])
-    res.json(deleteTodo.rows[0]);
-  } catch (err) {
-    console.lerror(err);
-  }
-})
 app.listen(3000, () => {
-  console.log("Server started on port 3000...");
-})
+  console.log('Server started on port 3000...');
+});
