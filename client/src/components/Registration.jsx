1a07b2c0ba2c1f525daaf768fbe30989b01b6793
mindychxn <mindychxn@gmail.com>
2024-03-27 12:04:49 -0400

    Create registration form component

---
+++ client/src/components/Registration.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+export default function Registration({ authenticate }) {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { username, email, password };
+      const res = await fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+
+      const parseRes = await res.json();
+
+      localStorage.setItem('token', parseRes.token);
+
+      authenticate(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  return (
+    <form className="flex flex-col" onSubmit={handleRegister}>
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
+}
