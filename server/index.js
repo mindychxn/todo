807c1a21f0ee7bcebb289518e42f935d6475659f
mindychxn <mindychxn@gmail.com>
2024-03-27 10:22:39 -0400

    Add dashboard route

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use('/auth', require('./routes/jwtAuth'));
 
 app.use('/todos', require('./routes/todos'));
 
+app.use('/dashboard', require('./routes/dashboard'));
+
 app.listen(3000, () => {
   console.log('Server started on port 3000...');
 });
