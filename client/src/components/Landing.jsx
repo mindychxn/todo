fb092e2d3ab6c880aaa72035252e9ee2493c5cdb
mindychxn <mindychxn@gmail.com>
2024-05-05 15:07:22 -0400

    Add linking to buttons and update to use GlassCard

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,5 +1,7 @@
 import './landing.css';
 import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import GlassCard from './GlassCard';
 
 export default function Landing() {
   const grinchTodos = [
@@ -11,25 +13,33 @@ export default function Landing() {
     'Wrestle with my self loathing',
   ];
   return (
-    <div className="landing-page w-screen h-screen flex justify-center items-center gap-10 p-10 text-[#405064] px-20">
+    <div className="landing-page w-screen h-screen flex justify-center items-center gap-10 py-10 text-charcoal px-20 min-w-min">
       <div className="w-full flex items-center fixed top-0 py-5 px-10">
         <div className="w-1/3">
           <img src={logo} className="h-[52px] aspect-square pb-2" />
         </div>
         <div className="w-1/3 font-bold text-4xl text-center">Taskify.</div>
-        <div className="flex justify-end gap-5 text-lg w-1/3">
-          <button className="font-medium py-2 px-4">Log In</button>
-          <button className="py-2 px-4 bg-[#FFD2D8] rounded-lg font-medium">Sign In</button>
+        <div className="flex justify-end gap-3 text-lg w-1/3">
+          <Link to={'/login'}>
+            <button className="font-medium py-2 px-4  transition ease-in-out duration-200 hover:text-charcoal/80 hover:scale-105">
+              Log In
+            </button>
+          </Link>
+          <Link to={'/register'}>
+            <button className="py-2 px-4 bg-babyPink rounded-lg font-medium hover:scale-105 hover:bg-darkPink transition ease-in-out duration-300">
+              Sign Up
+            </button>
+          </Link>
         </div>
       </div>
-      <div className="flex flex-col gap-6 w-1/2 w-min-fit">
+      <div className="flex flex-col gap-6 w-1/2 min-w-min">
         <div className="flex flex-col gap-6 font-bold text-6xl text-nowrap">
           Organize your day
           <div className="flex gap-2">
             with
             <div className="relative w-fit px-2">
               <span className="absolute w-full h-1/2 bg-white opacity-80 bottom-[9px] z-0" />
-              <span className="relative z-10 text-transparent bg-clip-text bg-gradient-to-r from-[#FFCCCC] via-[#D4B2FF] to-[#BBE2FF]">
+              <span className="relative z-10 text-transparent bg-clip-text bg-gradient-to-r from-babyPink via-babyPurple to-babyBlue">
                 Taskify
               </span>
             </div>
@@ -39,23 +49,25 @@ export default function Landing() {
           Take charge of your day, one to-do at a time! Seamlessly organize your tasks, set
           priorities, and track progress effortlessly.
         </div>
-        <button className="mt-2 w-fit py-2.5 px-6 bg-[#FFD2D8] rounded-lg text-xl font-medium">
-          Get Started
-        </button>
+        <Link to="./register">
+          <button className="mt-2 w-fit py-2.5 px-6 bg-babyPink rounded-lg text-xl font-medium hover:scale-105 transition ease-in-out duration-300 hover:bg-darkPink ">
+            Get Started
+          </button>
+        </Link>
       </div>
-      <div className="flex flex-col items-center justify-center bg-white/40 rounded-2xl py-12 px-14 gap-8 backdrop-blur-sm border-white/40 border-2 animate-float1">
+      <GlassCard className="flex-col gap-8 animate-float1 py-12 px-14">
         <div className="font-semibold text-[26px]">The Grinch's To Do List</div>
         <div className="text-xl space-y-8 w-full">
           {grinchTodos.map((todo, i) => (
             <div className="flex gap-4 items-center" key={i}>
-              <div class="mt-2 checkbox-wrapper-13 scale-150">
+              <div class="mt-2 checkbox-wrapper-13 scale-150 hover:scale-[1.6] transition">
                 <input id="c1-13" type="checkbox" />
               </div>
               {todo}
             </div>
           ))}
         </div>
-      </div>
+      </GlassCard>
     </div>
   );
 }
